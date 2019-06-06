b8dd7c8a3c041c42ee171087d4763afc2208ce68
Kehan Wang <wangkk@bu.edu>
2019-06-06 01:32:51 -0400

    update

--- PS4/app.js
+++ PS4/app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const bitcoinRouter = require('./routes/bitcoin');
+const bitcoinRouter = require('./routes/ps4');
 const app = express();
 
 // view engine setup
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bitcoin', bitcoinRouter);
+app.use('/ps4', bitcoinRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
