c5c06dcd851a12979c04090c93d58ec5eea9374e
Kehan Wang <wangkk@bu.edu>
2019-06-05 22:05:58 -0400

    almost finished

---
+++ PS4/routes/bitcoin.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
+const coinmarket = require('../_config/config.js')
+
+const getPrices = new Promise((resolve, reject) => {
+    const requestOptions = {
+        method: 'GET',
+        uri: coinmarket.ACCOUNT_SETTING_URL,
+        // qs: {
+        //     'id': '1'
+        // },
+        headers: {
+            'X-CMC_PRO_API_KEY': coinmarket.ACCESS_KEY
+        },
+        json: true,
+        gzip: true
+    };
+    rp(requestOptions)
+        .then(function (response) {
+            resolve(response.data);
+        })
+        .catch(function (err) {
+            reject(err)
+            
+        });
+})
+
+
+
+
+router.get('/', function (req, res, next) {
+    getPrices.then(response => {
+        res.render('bit', { price: response.BTC.quote.USD.price})
+        console.log('API call response:', response);
+    }).catch((err) => {
+        console.log('API call error:', err.message);
+    });
+})
+
+module.exports = router
