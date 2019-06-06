c9230a20e8c4d2387d33f60e3ffc0c1bd0644a6d
Kehan Wang <wangkk@bu.edu>
2019-06-05 22:12:31 -0400

    remove config

--- PS4/routes/bitcoin.js
+++ PS4/routes/bitcoin.js
@@ -40,3 +40,10 @@ router.get('/', function (req, res, next) {
 })
 
 module.exports = router
+
+// const coinmarketcap = {
+//     ACCESS_KEY: '896318b6-6c53-43db-9d9e-1cd27ca8f012',
+//     ACCOUNT_SETTING_URL: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC',
+// }
+// //
+// module.exports = coinmarketcap
