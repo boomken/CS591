18d9cc1d8727a629136028a1e906f3b992823d04
Kehan Wang <wangkk@bu.edu>
2019-06-08 00:41:28 -0400

    Update ps4.js

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -4,36 +4,25 @@ const router = express.Router();
 const rp = require('request-promise');
 const coinmarket = require('../_config.js')
 
-const getPrices = new Promise((resolve, reject) => {
-    const requestOptions = {
-        method: 'GET',
-        uri: coinmarket.ACCOUNT_SETTING_URL,
-        // qs: {
-        //     'id': '1'
-        // },
-        headers: {
-            'X-CMC_PRO_API_KEY': coinmarket.ACCESS_KEY
-        },
-        json: true,
-        gzip: true
-    };
-    rp(requestOptions)
-        .then(function (response) {
-            resolve(response.data);
-        })
-        .catch(function (err) {
-            reject(err)
-            
-        });
-})
 
-
+const requestOptions = {
+    method: 'GET',
+    uri: coinmarket.ACCOUNT_SETTING_URL,
+    // qs: {
+    //     'id': '1'
+    // },
+    headers: {
+        'X-CMC_PRO_API_KEY': coinmarket.ACCESS_KEY
+    },
+    json: true,
+    gzip: true
+};
 
 
 router.get('/', function (req, res, next) {
-    getPrices.then(response => {
-        res.render('bit', { price: response.BTC.quote.USD.price})
-        console.log('API call response:', response);
+    rp(requestOptions).then(response => {
+        res.render('bit', { price: response.data.BTC.quote.USD.price, })
+        console.log('API call response:', response.data.BTC.quote.USD.price);
     }).catch((err) => {
         console.log('API call error:', err.message);
     });
