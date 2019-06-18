ae1e725247ff380b7a34f5210e81ce707fb76d31
Kehan Wang <wangkk@bu.edu>
2019-06-17 20:32:37 -0400

    Update PS6

--- PS6/src/app/app.module.ts
+++ PS6/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ServiceComponent } from './service/service.component';
+import { ServiceComponent } from './service(PS6)/service.component';
 
 @NgModule({
   declarations: [
