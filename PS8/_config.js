944f801fb91b7d430b1f61b67b8bc60b64197ae9
Kehan Wang <wangkk@bu.edu>
2019-06-25 03:16:41 -0400

    Update PS8

---
+++ PS8/_config.js
@@ -0,0 +1,7 @@
+const coinmarketcap = {
+    ACCOUNT_SETTING_URL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=2000",
+    ACCESS_KEY: '896318b6-6c53-43db-9d9e-1cd27ca8f012',
+    mongoURL: "mongodb://localhost:27017/crypto",
+    CRYPTONEW_KEY: '1351d52fda3012a88281f7987126955a'
+}
+module.exports = coinmarketcap
---
+++ PS8/model/category_model.js
@@ -0,0 +1,15 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+module.exports = new Schema({
+    id: String,
+    name: String,
+    cmc_rank: Number,
+    slug: String,
+    cmc_id: Number,
+    source: String,
+    description: String,
+    type: String,
+    _category: String
+}, {collection: 'categories'})
