367678eb166d252931c8a9417c53a796f9d36c2c
Kehan Wang <wangkk@bu.edu>
2019-06-25 03:22:50 -0400

    Update PS8

--- PS8/routes/ps8.js
+++ PS8/routes/ps8.js
@@ -1,35 +1,4 @@
-// *****************************************************
-// *****************  PS4 ******************************
-// const express = require('express');
-// const router = express.Router();
-// const rp = require('request-promise');
-// const coinmarket = require('../_config.js')
-//
-//
-// const requestOptions = {
-//     method: 'GET',
-//     uri: coinmarket.ACCOUNT_SETTING_URL,
-//     // qs: {
-//     //     'id': '1'
-//     // },
-//     headers: {
-//         'X-CMC_PRO_API_KEY': coinmarket.ACCESS_KEY
-//     },
-//     json: true,
-//     gzip: true
-// };
-//
-//
-// router.get('/', function (req, res, next) {
-//     rp(requestOptions).then(response => {
-//         res.render('bit', { price: response.data.BTC.quote.USD.price, })
-//         console.log('API call response:', response.data.BTC.quote.USD.price);
-//     }).catch((err) => {
-//         console.log('API call error:', err.message);
-//     });
-// })
-//
-// module.exports = router
+
 // *****************************************************
 // const path = require('path');
 // console.log(__dirname);
@@ -154,3 +123,35 @@ router.get('/', async (req, res) => {
 // *****************************************************
 
 module.exports = router;
+// *****************************************************
+// *****************  PS4 ******************************
+// const express = require('express');
+// const router = express.Router();
+// const rp = require('request-promise');
+// const coinmarket = require('../_config.js')
+//
+//
+// const requestOptions = {
+//     method: 'GET',
+//     uri: coinmarket.ACCOUNT_SETTING_URL,
+//     // qs: {
+//     //     'id': '1'
+//     // },
+//     headers: {
+//         'X-CMC_PRO_API_KEY': coinmarket.ACCESS_KEY
+//     },
+//     json: true,
+//     gzip: true
+// };
+//
+//
+// router.get('/', function (req, res, next) {
+//     rp(requestOptions).then(response => {
+//         res.render('bit', { price: response.data.BTC.quote.USD.price, })
+//         console.log('API call response:', response.data.BTC.quote.USD.price);
+//     }).catch((err) => {
+//         console.log('API call error:', err.message);
+//     });
+// })
+//
+// module.exports = router
